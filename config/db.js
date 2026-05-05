55ce4e5fc5661df96e3f631c7e4420c07601267a
Ram Kumar U B <ramkumarbalakumar2004@gmail.com>
2026-05-05 10:31:09 +0530

    Update

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async() => {
     try {
-        // await mongoose.connect("mongodb+srv://ramkumarubaloinfotech_db_user:ram282004@cluster0.fbmaesh.mongodb.net/User")
-        await mongoose.connect("mongodb://localhost:27017/User")
+        await mongoose.connect("mongodb+srv://ramkumarubaloinfotech_db_user:ram282004@cluster0.fbmaesh.mongodb.net/User")
         console.log("Database Connected Successfully")
     }
     catch (err) {
